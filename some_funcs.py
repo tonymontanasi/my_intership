4645ffaf3b3ec393b4f84b359fb72d7218a7b063
anatoly <safrigin@tochka.com>
2024-01-28 01:32:22 +0500

    some funcs

---
+++ some_funcs.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Делитель не может быть нулем.")
+    return a / b
+
+
+print(add(3, 4))
